2ebad7ab4b8550b725cbfa2d36d73544a8b1b150
Santiago Suarez Ordoñez <santiycr@gmail.com>
2013-05-07 19:06:38 -0700

    dynaimcally change ffmpeg options based on its version + version bump

--- castro/__init__.py
+++ castro/__init__.py
@@ -5,6 +5,7 @@ from sys import stdout
 from time import sleep
 
 from multiprocessing import Process
+from subprocess import Popen, PIPE, STDOUT
 import yaml
 
 import lib.messageboard as mb
@@ -13,22 +14,24 @@ from lib.pyvnc2swf import vnc2swf
 # Get directory for storing files:
 DATA_DIR = os.environ.get('CASTRO_DATA_DIR') or tempfile.gettempdir()
 
+
 class CastroException(Exception):
     pass
 
+
 class Castro:
     def __init__(self,
-                 filename = "castro-video.swf",
-                 host     = "localhost",
-                 display  = 0,
-                 framerate = 12,
-                 freq = 1,
-                 clipping = None,
-                 port = None,
-                 passwd = os.path.join(os.path.expanduser("~"),
-                                       ".vnc", "passwd"),
-                 h264 = False,
-                 seconds_per_keyframe = 5):
+                 filename="castro-video.swf",
+                 host="localhost",
+                 display=0,
+                 framerate=12,
+                 freq=1,
+                 clipping=None,
+                 port=None,
+                 passwd=os.path.join(os.path.expanduser("~"),
+                                     ".vnc", "passwd"),
+                 h264=False,
+                 seconds_per_keyframe=5):
         self.filename = filename
         self.filepath = os.path.join(DATA_DIR, self.filename)
         self.host = host
@@ -50,11 +53,9 @@ class Castro:
         self.init()
 
     def init(self):
-        args=['lib/pyvnc2swf/vnc2swf.py',
-               '-n',
-               '-o', self.filepath,
-               '-R', 3,
-               '%s:%s' % (self.host, self.display) ]
+        args = ['lib/pyvnc2swf/vnc2swf.py',
+                '-n', '-o', self.filepath,
+                '-R', 3, '%s:%s' % (self.host, self.display)]
 
         # If password file is specified, insert it into args
         if self.passwd:
@@ -74,7 +75,7 @@ class Castro:
         if self.port:
             args.append(str(self.port))
 
-        self.recorder = Process(target= vnc2swf.main, args=[args])
+        self.recorder = Process(target=vnc2swf.main, args=[args])
 
     def start(self):
         self.recorder.start()
@@ -107,10 +108,22 @@ class Castro:
         The tip for adding the "-g" flag: http://www.infinitecube.com/?p=9
         """
 
+        version, _ = Popen(["ffmpeg", "-version"],
+                           stdout=PIPE, stderr=STDOUT).communicate()
+        if version.startswith("ffmpeg version 1"):
+            scale = "-qscale 0"
+        else:
+            scale = "-sameq"
         print "Running ffmpeg: encoding and creating keyframes"
-        cmd = "ffmpeg -y -i %s -g %s -sameq %s"
+        cmd = "ffmpeg -y -i %s -g %s " + scale + " %s"
         if self.h264:
-            cmd = "ffmpeg -y -i %s -vcodec libx264 -coder 0 -flags -loop -cmp +chroma -partitions -parti8x8-parti4x4-partp8x8-partb8x8 -me_method dia -subq 0 -me_range 16 -g %s -keyint_min 25 -sc_threshold 0 -i_qfactor 0.71 -b_strategy 0 -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -bf 0 -refs 1 -directpred 1 -trellis 0 -flags2 -bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree -wpredp 0 -aq_mode 0 -crf 30 %s"
+            cmd = ("ffmpeg -y -i %s -vcodec libx264 -coder 0 -flags -loop"
+                   " -cmp +chroma -partitions -parti8x8-parti4x4-partp8x8-partb8x8"
+                   " -me_method dia -subq 0 -me_range 16 -g %s -keyint_min 25"
+                   " -sc_threshold 0 -i_qfactor 0.71 -b_strategy 0 -qcomp 0.6"
+                   " -qmin 10 -qmax 51 -qdiff 4 -bf 0 -refs 1 -directpred 1 -trellis 0"
+                   " -flags2 -bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree -wpredp"
+                   " 0 -aq_mode 0 -crf 30 %s")
 
         os.system(cmd % (self.filepath,
                          self.framerate * self.seconds_per_keyframe,
@@ -128,32 +141,30 @@ class Castro:
     def cuepoint(self):
         print "\n\nCreating cuepoints:"
         # Create the cuepoints file
-        cuefile = open(self.cuefilepath,'w')
+        cuefile = open(self.cuefilepath, 'w')
 
         # Write the header
-        cuefile.write ("<?xml version=\"1.0\"?>\n")
-        cuefile.write ("<tags>\n")
-        cuefile.write ("  <!-- navigation cue points -->\n")
+        cuefile.write("<?xml version=\"1.0\"?>\n")
+        cuefile.write("<tags>\n")
+        cuefile.write("  <!-- navigation cue points -->\n")
 
         # Write the body
-        for i in range(0,self.duration,self.freq):
-            name = (datetime(1900,1,1,0,0,0) + timedelta(seconds=i)).strftime('%H:%M:%S')
-            cuefile.write ("  <metatag event=\"onCuePoint\">\n")
-            cuefile.write ("    <name>%s</name>\n" % name)
-            cuefile.write ("    <timestamp>%s000</timestamp>\n" % i)
-            cuefile.write ("    <type>navigation</type>\n")
-            cuefile.write ("  </metatag>\n")
-
+        for i in range(0, self.duration, self.freq):
+            name = (datetime(1900, 1, 1, 0, 0, 0)
+                    + timedelta(seconds=i)).strftime('%H:%M:%S')
+            cuefile.write("  <metatag event=\"onCuePoint\">\n")
+            cuefile.write("    <name>%s</name>\n" % name)
+            cuefile.write("    <timestamp>%s000</timestamp>\n" % i)
+            cuefile.write("    <type>navigation</type>\n")
+            cuefile.write("  </metatag>\n")
 
         # Write the footer
-        cuefile.write ("</tags>\n")
+        cuefile.write("</tags>\n")
         cuefile.close()
 
     def inject_metadata(self):
-        os.system("flvtool2 -AUt %s %s %s" %
-            (self.cuefilepath,
-             self.tempfilepath,
-             self.filepath))
+        os.system("flvtool2 -AUt %s %s %s"
+                  % (self.cuefilepath, self.tempfilepath, self.filepath))
 
     def cleanup(self):
         os.remove(self.cuefilepath)
@@ -171,14 +182,16 @@ class video:
     def __exit__(self, type, value, traceback):
         self.recorder.stop()
 
+
 # Show some output on screen during a test
 def countdown_timer():
     stdout.write("\nRecording a 10 second video...\n\n")
-    for i in range(10,0,-1):
+    for i in range(10, 0, -1):
         stdout.write("%s " % i)
         stdout.flush()
         sleep(1)
     stdout.write("\n")
+
 
 def test():
     c = Castro()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0.6'
+version = '1.0.8'
 long_description='''
 ------------
 Introduction
